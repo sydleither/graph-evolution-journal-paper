9fe9a73bd7aa5c697406df53341958f508b72f24
sydleither <sydleither@protonmail.com>
2024-05-30 17:16:54 -0400

    switch to more traditional p-values

--- pvalues_analysis.py
+++ pvalues_analysis.py
@@ -1,5 +1,4 @@
 import json
-from itertools import combinations
 import sys
 
 import matplotlib.pyplot as plt
@@ -33,47 +32,47 @@ def histograms(df, objectives, network_size):
     plt.close()
 
 
-def truncate_values(df, network_size):
-    num_decs = {10:2, 50:3, 100:4}
-    num_dec = num_decs[network_size]
+def truncate_values(df, objectives, num_dec=3):
     for objective in df.columns:
         if objective.endswith("distribution"):
-            df[objective] = df[objective].apply(lambda dist: tuple([round(x, num_dec-1) for x in dist]))
+            df[objective] = df[objective].apply(lambda dist: tuple([round(x, num_dec) for x in dist]))
+            objectives[objective] = tuple([round(x, num_dec) for x in objectives[objective]])
         else:
             df[objective] = df[objective].apply(lambda x: round(x, num_dec))
-    return df
+            objectives[objective] = round(objectives[objective], num_dec)
+    return df, objectives
 
 
 def calculate_pvalue(df, objective, target):
     num_samples = len(df)
     if len(df) == 0:
         return 0
-    if objective.endswith("distribution"):
+    #uniform distribution
+    if objective.endswith("distribution") or objective in ["connectance", "clustering_coefficient"]:
         return len(df[df[objective] == target])/num_samples
+    #normal distribution
+    elif objective in ["positive_interactions_proportion", 
+                       "average_positive_interactions_strength", 
+                       "average_negative_interactions_strength"]:
+        target = abs(target)
+        if target > 0.5:
+            return len(df[abs(df[objective]) >= target])/num_samples
+        else:
+            return len(df[abs(df[objective]) <= target])/num_samples
+    #right-heavy distribution
     else:
-        return len(df[abs(df[objective]) == abs(target)])/num_samples
+        return len(df[df[objective] >= target])/num_samples
 
 
-def significance_table(df, objectives, network_size):
-    df = truncate_values(df, network_size)
-    tables = {x:dict() for x in range(len(objectives)-1)}
-    reduced = {x:reduce_objective_name(x) for x in objectives}
+def pvalues(df, objectives, network_size):
+    df, objectives = truncate_values(df, objectives)
+    table = dict()
     for objective in objectives:
-        objectives_minus_curr = [x for x in objectives if x != objective]
-        for i in range(len(objectives_minus_curr)):
-            combos = list(combinations(objectives_minus_curr, i))
-            tables[i][objective] = dict()
-            for combo in combos:
-                df_combo = df
-                col_name = ""
-                for constraint in combo:
-                    contstraint_target = objectives[constraint]
-                    df_combo = df_combo.loc[df_combo[constraint] == contstraint_target]
-                    col_name += reduced[constraint]
-                pvalue = calculate_pvalue(df_combo, objective, objectives[objective])
-                tables[i][objective][col_name] = round(pvalue, 5)
+        contstraint_target = objectives[objective]
+        pvalue = calculate_pvalue(df, objective, contstraint_target)
+        table[objective] = round(pvalue, 5)
     with open(f"pvalues_{network_size}.json", "w") as f:
-        json.dump(tables, f, indent=4)
+        json.dump(table, f, indent=4)
 
 
 def fixed_histograms(df, objectives, network_size, constraints):
@@ -89,11 +88,9 @@ def main(network_size):
     df = pd.read_pickle(f"output/pvalues/df_{network_size}.pkl")
 
     histograms(df, objectives, network_size)
-    objectives["in_degree_distribution"] = tuple(objectives["in_degree_distribution"])
-    objectives["out_degree_distribution"] = tuple(objectives["out_degree_distribution"])
-    significance_table(df, objectives, network_size)
+    pvalues(df, objectives, network_size)
 
-    #fixed_histograms(df, objectives, network_size, ["in_degree_distribution"])
+    fixed_histograms(df, objectives, network_size, ["clustering_coefficient"])
 
 
 if __name__ == "__main__":
