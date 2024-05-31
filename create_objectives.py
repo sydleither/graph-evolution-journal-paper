4eeb4f1cacd11d4907377bc7df04098986d5ae52
sydleither <sydleither@protonmail.com>
2024-05-31 13:57:55 -0400

    standardize degree distribution targets

--- create_objectives.py
+++ create_objectives.py
@@ -5,45 +5,24 @@ import numpy as np
 from common import get_network_sizes
 
 
-def create_exp_dist(network_size):
-    #generate samples from exponential distribution
-    ys = {10:1, 50:0.1, 100:0.05}
-    y = ys[network_size]
-    basically_exp = [0]+[y*np.exp(-x*y) for x in range(1, network_size+1)]
-    #make distribution sum up to 1
-    sum_be = sum(basically_exp)
-    basically_exp = [x/sum_be for x in basically_exp]
-    #make distribution values achievable for the network size resolution
-    basically_exp_r = [(1/network_size)*round(x*network_size) for x in basically_exp]
-    #distribute rounding error across end of distribution
-    round_diff = (1/network_size)*round((sum(basically_exp) - sum(basically_exp_r))*network_size)
-    for i in range(1, network_size+1):
-        if basically_exp_r[i] == 0 and round_diff > 0:
-            j = 0
-            round_diff_remainder = round_diff
-            while round_diff_remainder > 0:
-                basically_exp_r[i+j] = 1/network_size
-                round_diff_remainder -= 1/network_size
-                j += 1
-            break
-        elif basically_exp_r[i] == 0 and round_diff < 0:
-            basically_exp_r[i-1] = basically_exp_r[i-1] + round_diff
-            break
-    #return a distribution where sum(p(k))=1 and the number of edges is an achievable integer
-    return basically_exp_r
+def create_dist(network_size):
+    edges = {0.3:0.2, 0.4:0.6, 0.5: 0.2}
+    degrees = [x/network_size for x in range(network_size+1)]
+    dd = [edges[x] if x in edges else 0 for x in degrees]
+    return dd
 
 
 def main():
     for network_size in get_network_sizes():
         objectives = {
-            "connectance": 0.3,
+            "connectance": 0.4,
             "clustering_coefficient": 0.4,
-            "positive_interactions_proportion": 0.7,
+            "positive_interactions_proportion": 0.75,
             "average_positive_interactions_strength": 0.3,
             "average_negative_interactions_strength": -0.3,
             "proportion_of_parasitism_pairs": 0.2,
-            "in_degree_distribution": create_exp_dist(network_size),
-            "out_degree_distribution": create_exp_dist(network_size)
+            "in_degree_distribution": create_dist(network_size),
+            "out_degree_distribution": create_dist(network_size)
         }
 
         with open(f"objectives_{network_size}.json", "w") as f:
--- supplement.py
+++ supplement.py
@@ -2,17 +2,18 @@ import sys
 
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 
 from common import get_network_sizes
-from create_objectives import create_exp_dist
+from create_objectives import create_dist
 
 
 def generate_dist_plots():
     fig, ax = plt.subplots(1, 3, figsize=(10, 5))
     for i,network_size in enumerate(get_network_sizes()):
-        dist = create_exp_dist(network_size)
+        dist = create_dist(network_size)
         ax[i].plot(list(range(network_size+1)), dist, linewidth=2, color="black")
         ax[i].set_title(f"Size {network_size} Network")
     fig.suptitle("Exponential Target Degree Distributions")
